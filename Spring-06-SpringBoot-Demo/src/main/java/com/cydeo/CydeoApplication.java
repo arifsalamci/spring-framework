c08867655518d583e02d0b63852c86e691be95ec
arifsalamci <arifsalamci@gmail.com>
2022-03-01 19:20:06 -0600

    Spring MVC Controller added

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import com.cydeo.model.Comment;
 import com.cydeo.service.CommentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,9 +11,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class CydeoApplication {
 
+    @Value("${name}")
+    private static String name;
+
     public static void main(String[] args) {
 
-
+        System.out.println(name);
         Comment comment = new Comment();
         comment.setAuthor("Mike");
         comment.setText("Spring Framework");
