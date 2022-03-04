137ab5464512675d5e46a5f1d42b8ebff1ba970a
arifsalamci <arifsalamci@gmail.com>
2022-03-03 19:44:56 -0600

    Spring MVC Model added

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/AnotherController.java
@@ -0,0 +1,28 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/another")
+public class AnotherController {
+
+    @RequestMapping("/list")
+    public String mentor(Model model){
+
+        List<Mentor> mentors = Arrays.asList(new Mentor("Mike", "Smith", 45, Gender.MALE),
+                new Mentor("Tom", "Hanks", 65, Gender.MALE),
+                new Mentor("Ammy", "Bryan", 25, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentors);
+
+        return "mentor/mentor";
+    }
+
+}
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/model/Gender.java
@@ -0,0 +1,7 @@
+package com.cydeo.model;
+
+public enum Gender {
+
+    FEMALE,
+    MALE;
+}
