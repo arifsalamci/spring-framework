efbf8b2dbe1d38bbbe7f1173725707675dbc8b7d
arifsalamci <arifsalamci@gmail.com>
2022-03-03 18:40:46 -0600

    Spring MVC Model added

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -12,6 +17,20 @@ public class StudentController {
 
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        int studentID = new Random().nextInt(1000);
+        model.addAttribute("id", studentID);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
         return "student/welcome";
     }
