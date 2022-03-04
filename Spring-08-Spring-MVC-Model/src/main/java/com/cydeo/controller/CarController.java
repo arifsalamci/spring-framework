4fb38827ad6bd7516bc997261ae366d5e4825602
arifsalamci <arifsalamci@gmail.com>
2022-03-03 20:12:11 -0600

    Spring MVC Model added

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,29 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, @RequestParam int year, Model model){
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        System.out.println(make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "Tesla") String make, Model model){
+
+        model.addAttribute("make", make);
+        System.out.println(make);
+        return "car/car-info";
+    }
+
+}
